9498464969687924b1ea851853f2962817c7359c
Anwar <anwarhussaini160@gmail.com>
2023-08-12 04:05:25 +0430

    x1339A_FillingDiamonds Codeforces

---
+++ Anwar/Module 01/Week 01/Day 02/x1339A_FillingDiamonds.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class x1339A_FillingDiamonds {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int test = input.nextInt();
+        while (test-- > 0) {
+            System.out.println(input.nextInt());
+        }
+    }
+
+}
